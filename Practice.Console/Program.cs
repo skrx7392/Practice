0d54bb546c16b94eab98f780c72ca2b3fc58aa53
PODDUTURI, SAI KRISHNA <skrx7392@gmail.com>
2020-01-07 18:31:12 -0600

    Add project files.

---
+++ Practice.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
